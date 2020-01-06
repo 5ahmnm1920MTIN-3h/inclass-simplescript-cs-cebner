99e9efd010e63ee7f81837d58846ad65bd278e16
Christian Ebner <Christian.Ebner@htl-salzburg.ac.at>
2020-01-06 19:27:59 +0100

    Script hinzugefügt. Inputfield funktioniert nicht.

---
+++ Assets/MyGame/Scripts/CommputeNumbers.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CommputeNumbers : MonoBehaviour
+{
+    public InputField ipVarA;
+    public InputField ipVarB;
+    public Text result;
+
+    float varA;
+    float varB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitVariables();
+    }
+
+    public void InitVariables()
+    {
+        varA = 0;
+        varB = 0;
+    }
+
+    public void SetResult()
+    {
+        float a = float.Parse(ipVarA.text);
+        float b = float.Parse(ipVarB.text);
+        //result.text = SubstractNumbers(a, b);
+        float resultAB = a - b;
+        result.text = resultAB.ToString();
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
